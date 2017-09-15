9619e710b21d9c3ca80166e36629b9bfcd0cf6ac
Albert H Mendrofa <albertmendrofa@gmail.com>
2017-09-15 09:37:29 +0700

    Create headertoko.h

---
+++ headertoko.h
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void lihatstok(int s1, int s2, int s3, int s4, int s5,int p1, int p2, int p3, int p4, int p5)
+{
+	system("cls");
+	printf("--Stok Barang--\n");
+	printf("No\tNama\t\tJumlah\tHarga\n");
+	printf("1\tPensil\t\t%d\t%d\n",s1,p1);
+	printf("2\tBolpen\t\t%d\t%d\n",s2,p2);
+	printf("3\tBinder\t\t%d\t%d\n",s3,p3);
+	printf("4\tKertas B5\t%d\t%d\n",s4,p4);
+	printf("5\tPenggaris\t%d\t%d\n",s5,p5);
+	printf("\n\n");
+	system("pause");
+}
+
+void beli(int s1, int s2, int s3, int s4, int s5,int p1, int p2, int p3, int p4, int p5)
+
+void bantuan()
+{
+	system("cls");
+	printf("--Bantuan--\n");
+	printf("Ini bantuan\n");
+	printf("\n\n");
+	system("pause");
+}
